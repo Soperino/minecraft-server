f3acf8dcd41b595458e967091e1f431c0b27f384
Soper <soper@soper.xyz>
2022-09-30 00:21:14 -0400

    removing unnecessary online mode jar option from start script

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 set jarfile=server_jar_name.jar
-java -Xmx4G -Xms2G -jar %jarfile% nogui -o true
+java -Xmx4G -Xms2G -jar %jarfile% --nogui
 PAUSE
--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 jarfile="JARFILE"
 cd "${0%/*}"
 echo "${0%/*}"
-java -Xmx4G -Xms2G -jar "$jarfile" nogui -o true
+java -Xmx4G -Xms2G -jar "$jarfile" --nogui
